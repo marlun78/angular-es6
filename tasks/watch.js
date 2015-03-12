c5bcb6aeaa9d8a780964bb6515e87d49cdf54d24
Martin Eneqvist <marlun78@users.noreply.github.com>
2015-03-12 09:07:49 +0100

    Update watch.js

--- tasks/watch.js
+++ tasks/watch.js
@@ -1,9 +1,10 @@
 'use strict';
 
-module.exports = function (gulp, config) {
-    return function () {
+module.exports = function module(gulp, config) {
+    function task() {
         gulp.watch(config.appDir + '**/*.js', ['scripts']);
         gulp.watch(config.appDir + '**/*.styl', ['stylus']);
         gulp.watch(config.appDir + 'index.html', ['html']);
-    };
+    }
+    return task;
 };
