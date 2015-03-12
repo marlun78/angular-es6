dd3feeae54ae4d192006ae9303c01f84b568d1cd
Martin Eneqvist <marlun78@users.noreply.github.com>
2015-03-12 09:16:55 +0100

    Update copy.js

--- tasks/copy.js
+++ tasks/copy.js
@@ -1,10 +1,11 @@
 'use strict';
 
 module.exports = function module(gulp, config) {
+    var files = './node_modules/angular/angular.js';
+    task.waitFor = ['clean'];
     function taks() {
-        return gulp.src('./node_modules/angular/angular.js')
+        return gulp.src(files)
             .pipe(gulp.dest(config.distDir + '/js'));
     }
-    task.waitFor = ['clean'];
     return task;
 };
