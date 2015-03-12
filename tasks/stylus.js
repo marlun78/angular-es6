4279b2d5f8b939e04f06dd57c1e6aeab937f7b3a
Martin Eneqvist <marlun78@users.noreply.github.com>
2015-03-12 09:07:09 +0100

    Update stylus.js

--- tasks/stylus.js
+++ tasks/stylus.js
@@ -1,16 +1,18 @@
 'use strict';
 
-var connect = require('gulp-connect');
-var sourcemaps = require('gulp-sourcemaps');
-var stylus = require('gulp-stylus');
+var $connect = require('gulp-connect');
+var $sourcemaps = require('gulp-sourcemaps');
+var $stylus = require('gulp-stylus');
 
 module.exports = function (gulp, config) {
-    return function () {
-        gulp.src(config.appDir + 'css/main.styl')
-            .pipe(sourcemaps.init())
-            .pipe(stylus({ compress: !config.isDebug }))
-            .pipe(sourcemaps.write())
+    function task() {
+        return gulp.src(config.appDir + 'css/main.styl')
+            .pipe($sourcemaps.init())
+            .pipe($stylus({ compress: !config.isDebug }))
+            .pipe($sourcemaps.write())
             .pipe(gulp.dest(config.distDir + 'css/'))
-            .pipe(connect.reload());
-    };
+            .pipe($connect.reload());
+    }
+    task.waitFor = ['clean'];
+    return task;
 };
