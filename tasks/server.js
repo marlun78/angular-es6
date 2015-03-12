8fee0781574788942d842a317b58b3e58b7776ca
Martin Eneqvist <marlun78@users.noreply.github.com>
2015-03-12 09:19:27 +0100

    Update server.js

--- tasks/server.js
+++ tasks/server.js
@@ -3,15 +3,12 @@
 var $connect = require('gulp-connect');
 
 module.exports = function (gulp, config) {
-    var options = {
-        root: config.distDir,
-        port: 3000,
-        livereload: true
-    };
-    
     function task() {
-        return $connect.server(options);
+        return $connect.server({
+            root: config.distDir,
+            port: 3000,
+            livereload: true
+        });
     }
-    
     return task;
 };
