c71ec400bd23f40bc27f569bdc7437d2245968fe
Martin Eneqvist <marlun78@users.noreply.github.com>
2015-03-16 22:11:37 +0100

    Update index.js

--- app/components/button/index.js
+++ app/components/button/index.js
@@ -1,6 +1,6 @@
-import * as directive from './buttonDirective';
+import * as buttonDirective from './buttonDirective';
 
 export const name = 'app.components.button';
 
 angular.module(name, [])
-    .directive(directive.name, directive.fn);
+    .directive(buttonDirective.name, buttonDirective.fn);
